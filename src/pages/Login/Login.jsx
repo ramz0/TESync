a93b4e09eba16d9847f679c957f5c91ca89b5494
ramz0 <luis.syberl@gmail.com>
2025-06-15 23:23:45 -0600

    Iconos dinamicos de Input

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,9 @@ import './LoginStyle.css'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+
 import Input from '../../components/Input/Input';
 
 export default function Login() {
@@ -16,15 +19,15 @@ export default function Login() {
   };
 
   return (
-    <div className='flex-row-center'>
+    <div className='flex-row-center contenedor-dec-form'>
       <span className='decoracion'>
         <img src="" alt="img-escuela" srcset="" />
       </span>
       <form onSubmit={handleLogin} className='form-login flex-column'>
         <h1 className='titulo-login'>Iniciar Sesión</h1>
         
-        <Input tipo={"email"} textoInterno={"Usuario."} hacer={e => setEmail(e.target.value)}></Input>
-        <Input tipo={"password"} textoInterno={"Contraseña."} hacer={e => setPassword(e.target.value)}></Input>
+        <Input icono={faUser} tipo={"email"} textoInterno={"Usuario."} hacer={e => setEmail(e.target.value)}></Input>
+        <Input icono={faKey} tipo={"password"} textoInterno={"Contraseña."} hacer={e => setPassword(e.target.value)}></Input>
 
         <button className='boton-n1' type="submit">Entrar</button>
       </form>
