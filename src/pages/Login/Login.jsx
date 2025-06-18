683bea762921b9a60d3fcb5631675f1087061074
ramz0 <luis.syberl@gmail.com>
2025-06-17 20:58:06 -0600

    Login con imagen de decoracion

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import './LoginStyle.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faKey } from '@fortawesome/free-solid-svg-icons';
@@ -20,21 +21,20 @@ export default function Login() {
   };
 
   return (
-
-    <div className='login-container-custom'>
-      <div className='login-image-custom'>
-        <img src={teschaImage} alt="img-escuela" />
+    <div className='flex-row-center login-container-custom'>
+      <div className='login-image-custom sombra'>
+        <img className='img-decoration' src={teschaImage} alt="img-escuela" />
       </div>
-      <div className='login-form-side'>
-        <form onSubmit={handleLogin} className='form-login'>
+        <form onSubmit={handleLogin} className='form-login flex-column'>
           <h1 className='titulo-login'>Iniciar Sesión</h1>
 
         <Input icono={faUser} tipo={"email"} textoInterno={"Usuario."} hacer={e => setEmail(e.target.value)}></Input>
         <Input icono={faKey} tipo={"password"} textoInterno={"Contraseña."} hacer={e => setPassword(e.target.value)}></Input>
-
-          <button className='boton-n1' type="submit">Entrar</button>
+        
+        <Link to="/profesor" className='boton-n1'>
+          <button>Entrar</button>
+        </Link>
         </form>
-      </div>
     </div>
   );
 }
