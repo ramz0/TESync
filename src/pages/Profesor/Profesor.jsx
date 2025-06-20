56aa8b0b89afba245d28ad76796f0a1f069d3438
ramz0 <luis.syberl@gmail.com>
2025-06-20 14:48:41 -0600

    Prueba de botones

--- src/pages/Profesor/Profesor.jsx
+++ src/pages/Profesor/Profesor.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom"
 
+import BotonMaterias from "../../components/BotonMaterias/BotonMaterias"
+
 const Profesor = () => {
   return (
     <>
       <h1>Soy un Profesor</h1>
+      <BotonMaterias estado={'pendiente'} materia={'Base De Datos'} grupo={'4852'}></BotonMaterias>
+      <BotonMaterias estado={'listo'} materia={'Base De Datos'} grupo={'4851'}></BotonMaterias>
     </>
   )
 }
