4ca1dc66af8dac3a5f26c05bc285f1ad96365eaa
ramz0 <luis.syberl@gmail.com>
2025-06-20 20:44:45 -0600

    Nuevo diseño de Tabla global

---
+++ src/components/TablaProfesores/TablaProfesores.jsx
@@ -0,0 +1,23 @@
+
+const TablaProfesores = () => {
+  return (
+    <>
+      <table>
+        <tr>
+          <th>nombre</th>
+          <th>cedula</th>
+          <th>correo</th>
+          <th>materias</th>
+        </tr>
+        <tr>
+          <td>Luis</td>
+          <td>1212</td>
+          <td>luis@ramz.com</td>
+          <td>muchas</td>
+        </tr>
+      </table>
+    </>
+  )
+}
+
+export default TablaProfesores;
