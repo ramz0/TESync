7cdd8193592d589329bcbe814e6e04c7b6ee6dff
ramz0 <luis.syberl@gmail.com>
2025-06-21 13:07:00 -0600

    La estructura del archivo esta mas limpia

--- src/pages/Profesor/Profesor.jsx
+++ src/pages/Profesor/Profesor.jsx
@@ -1,12 +1,6 @@
 import './ProfesorStyle.css'
-<<<<<<< HEAD
-import { useState } from 'react';
-import { faFileExcel } from '@fortawesome/free-solid-svg-icons';
-import InfoCard from '../../components/infocard/Infocard.jsx';
-=======
 
 import CardAlumno from '../../components/infocard/CardAlumno.jsx';
->>>>>>> temp-fixes
 import ListaMateriasProfesor from '../../components/ListaMateriasProfesor/ListaMateriasProfesor.jsx';
 import Dashboard from '../dashboard/Dashboard';
 import CalificacionesAlumnos from '../../components/CalificacionesAlumnos/CalificacionesAlumnos.jsx';
