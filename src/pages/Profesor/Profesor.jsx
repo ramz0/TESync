3aaf9377f832a32ef79dad035354ee3d6785d8f5
ramz0 <luis.syberl@gmail.com>
2025-06-21 16:58:48 -0600

    La vista esta mas organizada y tenemos varios estados para los componentes

--- src/pages/Profesor/Profesor.jsx
+++ src/pages/Profesor/Profesor.jsx
@@ -1,5 +1,8 @@
 import './ProfesorStyle.css'
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 import CardAlumno from '../../components/infocard/CardAlumno.jsx';
 import ListaMateriasProfesor from '../../components/ListaMateriasProfesor/ListaMateriasProfesor.jsx';
@@ -8,18 +11,27 @@ import CalificacionesAlumnos from '../../components/CalificacionesAlumnos/Califi
 
 const Profesor = () => {
   const [editarCalificaciones, setEditiarCalificaciones] = useState(false);
+  const [ocultarBtn, setOcultarBtn] = useState(false);
+  const [iconBar, setIconBar] = useState("");
 
-  const mostrarComponente = () => {
+  const mostrarComponente = () => setEditiarCalificaciones(true)
+
+  const mostrarOcultarComponente = () => {
     setEditiarCalificaciones(!editarCalificaciones)
-    console.log(`Se cambio el valor de editarCalificaicones: ${editarCalificaciones}`)
-  }
+    setOcultarBtn(true)
+    setIconBar(editarCalificaciones?"hide-bar":"")
+  } 
 
   return (
     <main className='fondo-Profesor'>
       <Dashboard />
       {/* <CardAlumno /> */}
       <ListaMateriasProfesor mostrarComponente={mostrarComponente}/>
-      <button onClick={mostrarComponente}>Show</button>
+      {ocultarBtn||editarCalificaciones ? (
+        <button data-aos="fade-up" className={'show-bar ' + iconBar} onClick={mostrarOcultarComponente}>
+          <FontAwesomeIcon icon={editarCalificaciones ? faAngleUp : faAngleDown} />
+        </button>
+      ) :(<></>)}
       {editarCalificaciones ? (<CalificacionesAlumnos />) : (<></>)}
     </main>
   );
