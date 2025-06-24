16b1950bcce6cb4e217958d1bd6154984e884806
ramz0 <luis.syberl@gmail.com>
2025-06-23 23:43:35 -0600

    Implementacion de servicio Backend Tesync

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import './LoginStyle.css';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { apiTesync } from '../../servicios/axios.js';
 
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faKey } from '@fortawesome/free-solid-svg-icons';
@@ -10,14 +10,22 @@ import Input from '../../components/Input/Input';
 import teschaImage from '../../assets/tescha.jpg';
 
 export default function Login() {
-  const [email, setEmail] = useState('');
+  const [matricula, setMatricula] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
 
-  const handleLogin = (e) => {
+  const rutasApiTesync = {
+    loginAlumno: 'http://localhost:3000/api/alumnos/login',
+    loginProfesor: 'http://localhost:3000/api/maestros/login',
+  }
+
+  const iniciarAlumno = async (e) => {
     e.preventDefault();
-    console.log('Iniciar sesión con:', email, password);
-    navigate('/');
+    try {
+      const response = await apiTesync.post(rutasApiTesync.loginAlumno, { matricula, password });
+      console.log("Respuesta del backend:", response.data);
+    } catch (error) {
+      console.error("Error:", error.response?.data);
+    }
   };
 
   return (
@@ -25,17 +33,17 @@ export default function Login() {
       <div className='login-image-custom sombra'>
         <img className='img-decoration' src={teschaImage} alt="img-escuela" />
       </div>
-        <form data-aos="fade-left" onSubmit={handleLogin} className='form-login flex-column'>
+        <form data-aos="fade-left" onSubmit={iniciarAlumno} className='form-login flex-column'>
           <h1 className='titulo-login'>Iniciar Sesión</h1>
           <main className='flex-column contenido-login'>
-            <Input icono={faUser} tipo={"email"} textoInterno={"Usuario."} hacer={e => setEmail(e.target.value)} />
+            <Input icono={faUser} tipo={"matricula"} textoInterno={"Usuario."} hacer={e => setMatricula(e.target.value)} />
             <Input icono={faKey} tipo={"password"} textoInterno={"Contraseña."} hacer={e => setPassword(e.target.value)} />
             
-            <Link to="/profesor" className='boton-n2'>
+            <Link type='submint' to="/profesor" className='boton-n2'>
               Entrar Prof
             </Link>
 
-            <Link to="/alumno" className='boton-n2'>
+            <Link type='submint' to="/alumno" className='boton-n2'>
               Entrar Alum
             </Link>
 
--- src/servicios/axios.js
+++ src/servicios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({
+export const apiTesync = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: { "Content-Type": "application/json" },
 });
